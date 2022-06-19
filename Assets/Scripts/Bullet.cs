df6f28b1522e583925868edd53a36a6c5b28608d
Reknor <33392647+Reknor@users.noreply.github.com>
2022-06-19 10:59:12 +0200

    Added second enemy gog

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -5,7 +6,9 @@ public class Bullet : MonoBehaviour
     public float speed;
     public Rigidbody2D rb;
     public int dmg = 1;
-    
+
+    private const float Lifetime = 10f;  // Time after which bullet is destroyed
+
     void Start()
     {
         rb.velocity = transform.right * speed;
@@ -15,4 +18,10 @@ public class Bullet : MonoBehaviour
     {
         Destroy(gameObject);
     }
+    private IEnumerator DeleteAfterLifetime(float lifetime)
+    {
+        yield return new WaitForSeconds(lifetime);
+        Destroy(gameObject);
+    }
+
 }
---
+++ Assets/Scripts/FireballScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class FireballScript : MonoBehaviour
+{
+    public Vector2 direction;
+    public int damage;
+    public float moveSpeed = 6;
+    public PlayerMovementScript player;
+
+    private const float Lifetime = 10f;  // Time after which fireball is destroyed
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        StartCoroutine(DeleteAfterLifetime(Lifetime));
+    }
+
+    private IEnumerator DeleteAfterLifetime(float lifetime)
+    {
+        yield return new WaitForSeconds(lifetime);
+        Destroy(gameObject);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = (Vector2)transform.position + (direction * moveSpeed * Time.deltaTime);
+        rb.MovePosition(position);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
